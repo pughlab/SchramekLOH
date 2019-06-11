d936d5b98085598e99fe651e15b944c40e53e106
Rene Quevedo <rquevedo@uhnresearch.ca>
2019-06-11 18:13:58 -0400

    New function loaded from stack overflow.  Locates NA's in a vector and replaces it with the closest non-NA element in the vector

---
+++ SchramekLOH/R/fixNA.R
@@ -0,0 +1,27 @@
+#' fixNA
+#' 
+#' @description https://stackoverflow.com/questions/10077415/replacing-nas-in-r-with-nearest-value
+#' @param dat A vector
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fixNA <- function(dat) {
+  N <- length(dat)
+  na.pos <- which(is.na(dat))
+  if (length(na.pos) %in% c(0, N)) {
+    return(dat)
+  }
+  non.na.pos <- which(!is.na(dat))
+  intervals  <- findInterval(na.pos, non.na.pos,
+                             all.inside = TRUE)
+  left.pos   <- non.na.pos[pmax(1, intervals)]
+  right.pos  <- non.na.pos[pmin(N, intervals+1)]
+  left.dist  <- na.pos - left.pos
+  right.dist <- right.pos - na.pos
+  
+  dat[na.pos] <- ifelse(left.dist <= right.dist,
+                        dat[left.pos], dat[right.pos])
+  return(dat)
+}
