8bc0bbd7dd8e34a56e661ba693558ca1c827b28d
Rene Quevedo <rquevedo@uhnresearch.ca>
2019-10-30 12:43:50 -0400

    Added the gene functional impact data to a loadable dataset in the R package

--- SchramekLOH/R/data.R
+++ SchramekLOH/R/data.R
@@ -12,3 +12,18 @@
 #' @source \url{http://www.diamondse.info/}
 "snp6"
 
+#' Prices of 50,000 round cut diamonds.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"gene.dat"
+
+
